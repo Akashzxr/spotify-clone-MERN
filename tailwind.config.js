c74e420dee84aff15690b0e157cf997c10841c68
akashks <akashks8855@gmail.com>
2024-10-14 12:19:05 +0530

    added a color for the navbar search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
     extend: {},
     fontFamily:{
       'spotifytitle': ['spotifytitle'],
-    }
+    },
+    colors: {
+      'navbar-gray': '#1f1f1f',
+    },
   },
   plugins: [],
 }
